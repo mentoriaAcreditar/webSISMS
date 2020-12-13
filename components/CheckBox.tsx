3d83d6f138bb02ff5529226132763f82809aca93
joão  mariano <joao.mariano74@gmail.com>
2020-12-13 15:07:34 -0300

    Removendo algumas erros que o ts tava acusando

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,15 +1,12 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
-const CheckBox = ({name, placeholder, id, value}) => {
+const CheckBox = ({ name, placeholder, id, value }: any) => {
   return (
     <div className="flex flex-row items-center space-x-2 ">
-    
-          
-            <input type="radio" id={id} name={name} value={value}/>
-            <label for={name}>{value}</label>
-           
+      <input type="radio" id={id} name={name} value={value} />
+      <label htmlFor={name}>{value}</label>
     </div>
   );
-}
+};
 
 export default CheckBox;
--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -8,19 +8,18 @@ const Nav = ({ fixed }: any) => {
       <nav className="fixed w-full flex flex-wrap bg-blue items-center shadow-md  px-2 py-3 navbar-expand-lg  mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full  flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <Link
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-              href="#"
-            >
-              <img className="w-24" src="./Logo.svg" alt="" />
-            </Link>
+            <div className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white">
+              <Link href="#">
+                <img className="w-24" src="./Logo.svg" alt="" />
+              </Link>
+            </div>
             <button
               className=" cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
               <svg
-                class="fill-current  h-6 w-6"
+                className="fill-current  h-6 w-6"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -64,7 +63,7 @@ const Nav = ({ fixed }: any) => {
               <li className="nav-item">
                 <Link href="profile">
                   <img
-                    class="cursor-pointer w-12 ml-2"
+                    className="cursor-pointer w-12 ml-2"
                     src="./user.svg"
                     alt=""
                   />
