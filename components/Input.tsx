8bc32f67c8b90c3cd4d58fc7fcc38e93773fdfe4
joão  mariano <joao.mariano74@gmail.com>
2020-12-31 01:07:11 -0300

    add modificadores para os input

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,7 +4,13 @@ interface IInput extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder: string;
 }
-const Input: React.FC<IInput> = ({ name, placeholder }: IInput) => {
+const Input: React.FC<IInput> = ({
+  name,
+  placeholder,
+  id,
+  value,
+  onChange,
+}: IInput) => {
   return (
     <div className="flex flex-col p-2  ">
       <label className="text-green mx-2" htmlFor="#">
@@ -12,6 +18,9 @@ const Input: React.FC<IInput> = ({ name, placeholder }: IInput) => {
       </label>
       <input
         placeholder={placeholder}
+        id={id}
+        value={value}
+        onChange={onChange}
         className="p-4 h-10  bg-input box-border  text-green bg-input border-color rounded-3xl"
         type="text"
       />
