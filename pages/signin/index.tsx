e75f8ca98b6a78e8359c70e39911f24e59044edd
joão  mariano <joao.mariano74@gmail.com>
2020-12-31 01:11:23 -0300

    remove  import de styles main que tava dando erro

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-import "../../styles/main.css";
 
 import Input from '../../components/Input';
 const Login = ({type}) => {
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-import "../../styles/main.css";
+
 
 import Input from '../../components/Input';
 
